19b8445e528525142915ef5dcbd0afe856d55926
FireDemon450 <117426318+FireDemon450@users.noreply.github.com>
2026-03-24 16:16:10 +0000

    made code

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+
+public class Greeting { 
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in); 
+
+        System.out.print("Enter your first name: "); 
+        String name = input.nextLine(); 
+
+        System.out.println("Hello, " + name + "!"); 
+        System.out.println("Your name has " + name.length() + " 
+    characters."); 
+    }
+}
